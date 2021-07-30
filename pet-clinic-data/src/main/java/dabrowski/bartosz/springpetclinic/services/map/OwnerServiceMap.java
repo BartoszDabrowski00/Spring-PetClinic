5bcb4c638af960ff3c3b7dcd6b591bf654622bb7
Bartek <bartosz.dabrowski2000@gmail.com>
2021-07-30 18:34:54 +0200

    implemented map based services

---
+++ pet-clinic-data/src/main/java/dabrowski/bartosz/springpetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,34 @@
+package dabrowski.bartosz.springpetclinic.services.map;
+
+import dabrowski.bartosz.springpetclinic.model.Owner;
+import dabrowski.bartosz.springpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+}
