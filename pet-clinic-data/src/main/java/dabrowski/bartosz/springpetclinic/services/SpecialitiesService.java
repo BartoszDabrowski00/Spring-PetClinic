f2d86addc1db0bc87f5e935fd822b0de1112d87a
Bartek <bartosz.dabrowski2000@gmail.com>
2021-08-08 14:35:51 +0200

    added specialities map service

---
+++ pet-clinic-data/src/main/java/dabrowski/bartosz/springpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package dabrowski.bartosz.springpetclinic.services;
+
+import dabrowski.bartosz.springpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long>{
+}
