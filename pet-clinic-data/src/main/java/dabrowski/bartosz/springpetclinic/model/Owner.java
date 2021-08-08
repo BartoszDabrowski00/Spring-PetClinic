820a447516fecafaa42d847cf392a921110f3749
Bartek <bartosz.dabrowski2000@gmail.com>
2021-08-08 13:31:07 +0200

    added visit, updated owner with pet relationship

--- pet-clinic-data/src/main/java/dabrowski/bartosz/springpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/dabrowski/bartosz/springpetclinic/model/Owner.java
@@ -1,6 +1,16 @@
 package dabrowski.bartosz.springpetclinic.model;
 
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
 public class Owner extends Person{
+
+    private Set<Pet> pets;
+
 
 }
