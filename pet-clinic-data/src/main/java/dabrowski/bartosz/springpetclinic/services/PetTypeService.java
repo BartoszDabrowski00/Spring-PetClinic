9e2fb9d4e84447749425ba39a20c0a54c21a40ee
Bartek <bartosz.dabrowski2000@gmail.com>
2021-08-08 13:43:40 +0200

    added pet type service

---
+++ pet-clinic-data/src/main/java/dabrowski/bartosz/springpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package dabrowski.bartosz.springpetclinic.services;
+
+import dabrowski.bartosz.springpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+}
