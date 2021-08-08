c2b2fda3674d237a9be0a2e2b77464862cfa8a58
Bartek <bartosz.dabrowski2000@gmail.com>
2021-08-08 19:48:39 +0200

    fixed broken links

--- pet-clinic-web/src/main/java/dabrowski/bartosz/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/dabrowski/bartosz/springpetclinic/bootstrap/DataLoader.java
@@ -30,7 +30,7 @@ public class DataLoader implements CommandLineRunner {
 
         int count = petTypeService.findAll().size();
 
-        if (count==0){
+        if (count == 0) {
             loadData();
         }
     }
--- pet-clinic-web/src/main/java/dabrowski/bartosz/springpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/dabrowski/bartosz/springpetclinic/controllers/VetController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping({"/vets","/vets.html"})
+@RequestMapping({"/vets", "/vets.html"})
 @Controller
 public class VetController {
 
@@ -15,8 +15,8 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"","/","/index","/index.html"})
-    public String listVets(Model model){
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listVets(Model model) {
 
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
