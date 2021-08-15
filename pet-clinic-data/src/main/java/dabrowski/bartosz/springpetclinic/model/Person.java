d3905b2109cce27e7964277ddbe652eedab9cbb4
Bartek <bartosz.dabrowski2000@gmail.com>
2021-08-15 15:41:29 +0200

    updated owner, pet, pettype entities with jpa

--- pet-clinic-data/src/main/java/dabrowski/bartosz/springpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/dabrowski/bartosz/springpetclinic/model/Person.java
@@ -4,12 +4,19 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
 @Getter
 @Setter
 @NoArgsConstructor
+@MappedSuperclass
 public class Person extends BaseEntity{
 
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
 
 }
