0902e0e7250e73e80dc03722f38df085f455f7be
Bartek <bartosz.dabrowski2000@gmail.com>
2021-08-15 16:44:35 +0200

    added spring data jpa service for pettypes

---
+++ pet-clinic-data/src/main/java/dabrowski/bartosz/springpetclinic/services/springdatajpa/PetTypeSDJpaService.java
@@ -0,0 +1,46 @@
+package dabrowski.bartosz.springpetclinic.services.springdatajpa;
+
+import dabrowski.bartosz.springpetclinic.model.PetType;
+import dabrowski.bartosz.springpetclinic.repositories.PetTypeRepository;
+import dabrowski.bartosz.springpetclinic.services.PetTypeService;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+@AllArgsConstructor
+public class PetTypeSDJpaService implements PetTypeService {
+
+    private final PetTypeRepository petTypeRepository;
+
+    @Override
+    public Set<PetType> findAll() {
+        Set<PetType> types = new HashSet<>();
+        petTypeRepository.findAll().forEach(types::add);
+        return types;
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return petTypeRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return petTypeRepository.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        petTypeRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petTypeRepository.deleteById(id);
+    }
+}
