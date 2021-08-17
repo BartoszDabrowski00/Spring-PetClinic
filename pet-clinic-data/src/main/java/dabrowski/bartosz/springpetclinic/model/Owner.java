f950ca01a52548c67e8aaa678652430bde91f690
Bartek <bartosz.dabrowski2000@gmail.com>
2021-08-17 12:00:46 +0200

    refactored lombok

--- pet-clinic-data/src/main/java/dabrowski/bartosz/springpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/dabrowski/bartosz/springpetclinic/model/Owner.java
@@ -1,8 +1,7 @@
 package dabrowski.bartosz.springpetclinic.model;
 
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.HashSet;
@@ -10,6 +9,9 @@ import java.util.Set;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "owners")
 public class Owner extends Person{
