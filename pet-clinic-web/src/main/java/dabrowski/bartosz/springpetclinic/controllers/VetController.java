c1c0ba0b001198023862a56c4187c8c7badd791e
Bartek <bartosz.dabrowski2000@gmail.com>
2021-09-02 14:49:26 +0200

    added json endpoint for vets

--- pet-clinic-web/src/main/java/dabrowski/bartosz/springpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/dabrowski/bartosz/springpetclinic/controllers/VetController.java
@@ -1,10 +1,15 @@
 package dabrowski.bartosz.springpetclinic.controllers;
 
+import dabrowski.bartosz.springpetclinic.model.Vet;
 import dabrowski.bartosz.springpetclinic.services.VetService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @RequestMapping({"/vets", "/vets.html"})
 @Controller
@@ -13,10 +18,15 @@ public class VetController {
 
     private final VetService vetService;
 
-    @RequestMapping({"", "/", "/index", "/index.html"})
+    @GetMapping({"", "/", "/index", "/index.html"})
     public String listVets(Model model) {
 
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping("/api")
+    public @ResponseBody Set<Vet> getVetsJson(){
+        return vetService.findAll();
     }
 }
